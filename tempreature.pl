b06c40f65a8e47704e282d241429b9cbb253289f
Dragos Trif <drd.trif@gmail.com>
2016-05-31 20:51:13 +0300

    basic regex expresion that contains the folowing elements : - ^$ start and end of the line metacharacters - #[] charcter class - ?*+ quantifiers - \. escape sequnce

--- tempreature.pl
+++ tempreature.pl
@@ -4,9 +4,16 @@ use warnings;
 
 
 print "Enter a tempearture in celsius:\n";
-my $celsius =  <>;
-chop $celsius;
-
+my $celsius =  <>;# this will read one line from the user
+chop $celsius;# this removes the ending new line from $celsius
+#elements of the regex:
+# =~  should be read as "matches"
+# ^$ start and end of the line metacharacters
+#[] charcter class
+#? one elemnt allowed; none required
+#+ one element required; more allowed
+#* unlimited elements allowed; none required
+# \. escape sequnce for dot metacharacter 
 if( $celsius =~ m/^[+-]?[0-9]+(\.[0-9]*)?$/ ){
 	my $farenheit = ($celsius * 9 / 5) + 32;
 	#print "$celsius C = $farenheit F\n";
