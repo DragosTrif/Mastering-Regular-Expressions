17173f2f3604ba603750a94da07aed25f3148359
Dragos Trif <drd.trif@gmail.com>
2016-06-01 19:09:47 +0300

    grouping and capturing inside a regex with (), \s metacharacter and /i modifier

--- temperature_converter_1.pl
+++ temperature_converter_1.pl
@@ -8,8 +8,10 @@ chop $input;
 #new regex concepts :
 # () used for capturing and  grouping
 # and $1 $2 for retrevening the captured values 
+#\s matches any whitespace characters
+# m// followed by /i instructs the regex to be case insenstive 
 
-if( $input =~ /^([-+]?[0-9]+(\.[0-9]*)?)([CF])$/) 
+if( $input =~ m/^([-+]?[0-9]+(\.[0-9]*)?)\s*([CF])$/i) 
 {                                                         
 	my $input_number = $1; #retreving values 
 	my $input_type   = $3; #because we used () for grouping  $input_type  it's hold in $3 not $2
